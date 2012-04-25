123e604b1b59bb74cdec580e294de43fc32ba881
Vincent Siebert <vincent@siebert.im>
2012-04-25 12:32:00 +0100

    fixing route problem in ems

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,10 @@
 Ems::Engine.routes.draw do
+  get "ems/index"
+  root :to => "ems#index"  
   resources :reports
-
   resources :news
-
   resources :articles
-
   resources :tags
   resources :channels
   resources :categories
-
-  get "ems/index"
-
-  root :to => "ems#index"
 end
