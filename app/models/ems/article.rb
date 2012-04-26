f0ef5017e3125db2b3783428ecf1776f1031fb2f
Vincent Siebert <vincent@siebert.im>
2012-04-26 12:14:22 +0100

    adding paperclip file to article

--- app/models/ems/article.rb
+++ app/models/ems/article.rb
@@ -30,7 +30,10 @@ module Ems
     has_and_belongs_to_many :news, :join_table => 'ems_articles_news'
     accepts_nested_attributes_for :news
     has_and_belongs_to_many :reports, :join_table => 'ems_articles_reports'
-    accepts_nested_attributes_for :reports    
+    accepts_nested_attributes_for :reports
+    # paperclip files
+    has_attached_file :image564x252, :styles => { :image312x189 => "312x129", :image312x126 => "312x126", :image228x126 => "228x126" }
+    
 
     # Method to make sure we have all our default values set on the object
     def init
