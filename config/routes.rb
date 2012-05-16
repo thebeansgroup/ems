3f3088a3531b7f5097eb6f390c48c238f29d867f
Vincent Siebert <vincent@siebert.im>
2012-05-16 16:21:11 +0100

    optimising routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,18 +2,8 @@ Ems::Engine.routes.draw do
   root :to => "ems#index"
   
   # insight rules
-  resources :category do
-    resources :articles
-    resources :news
-    resources :reports
-  end
-  
-  resources :articles
-  resources :news
-  resources :reports
-  resources :tags
-  resources :channels
-  resources :categories
-  resources :images
-  resources :media
+  resources :categories do
+    resources :articles, :news, :reports
+  end  
+  resources :articles, :news, :reports, :tags, :channels, :categories, :images, :media
 end
