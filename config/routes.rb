ef7067b8f034398f5827508b80cac1b1f8e35557
Vincent Siebert <vincent@siebert.im>
2012-05-18 14:28:53 +0100

    updating the routeing

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Ems::Engine.routes.draw do
   root :to => "ems#index"
   
-  resources :articles, :news, :reports, :only => [:update, :create, :destroy]
+  resources :articles, :news, :reports
   # insight rules
   resources :categories, :only => [:index, :show] do
-    resources :articles, :news, :reports, :only => [:index, :new, :show, :edit]
+    resources :articles, :news, :reports
   end  
   resources :tags, :channels, :images, :media
 end
