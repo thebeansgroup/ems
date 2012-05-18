64e576fd40949625f0090574a7bcc5559f7942e9
Vincent Siebert <vincent@siebert.im>
2012-05-18 14:12:47 +0100

    updating templates

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Ems::Engine.routes.draw do
   root :to => "ems#index"
   
-  resources :articles, :news, :reports# , :only => [:update, :create, :destroy]
+  resources :articles, :news, :reports, :only => [:update, :create, :destroy]
   # insight rules
-  resources :categories do
-    resources :articles, :news, :reports# , :only => [:index, :new, :show, :edit]
+  resources :categories, :only => [:index, :show] do
+    resources :articles, :news, :reports, :only => [:index, :new, :show, :edit]
   end  
-  resources :tags, :channels, :categories, :images, :media
+  resources :tags, :channels, :images, :media
 end
