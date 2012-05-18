d4c390f587695843143df26f903888e9f586baf6
Vincent Siebert <vincent@siebert.im>
2012-05-18 12:57:02 +0100

    updating routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Ems::Engine.routes.draw do
   root :to => "ems#index"
   
+  resources :articles, :news, :reports# , :only => [:update, :create, :destroy]
   # insight rules
   resources :categories do
-    resources :articles, :news, :reports
+    resources :articles, :news, :reports# , :only => [:index, :new, :show, :edit]
   end  
-  resources :articles, :news, :reports, :tags, :channels, :categories, :images, :media
+  resources :tags, :channels, :categories, :images, :media
 end
