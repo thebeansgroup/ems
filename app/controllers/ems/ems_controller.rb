5c427bcd3784e62c4e94b6b2b906d09149de0674
Vincent Siebert <vincent@siebert.im>
2012-05-21 19:07:20 +0100

    adding activeadmin ems workaround

--- app/controllers/ems/ems_controller.rb
+++ app/controllers/ems/ems_controller.rb
@@ -3,6 +3,11 @@ module Ems
     skip_authorization_check :only => [:index]
         
     def index
+      # Workaround for active admin routing problem.
+      # see:
+      # http://stackoverflow.com/questions/10684566/mountable-engine-routing-with-active-admin-on-rails
+      # @todo find a nicer solution for this problem.
+      render :layout => 'ems/application'
     end
     
   end
