fbde2805c751a5cf4bfac8582f07c96b2ef4b18d
Vincent Siebert <vincent@siebert.im>
2012-05-22 17:35:17 +0100

    removing type restrictions from assets

--- app/models/ems/asset.rb
+++ app/models/ems/asset.rb
@@ -6,7 +6,7 @@ module Ems
           
     belongs_to :assetable, :polymorphic => true
     
-    has_attached_file :asset, :styles =>{:original => "564x252>"}
+    has_attached_file :asset, :styles => {:original => "564x252>"}
     
     # virtual alt text accessor - returns the title of the image otherwise the path
     def alt
