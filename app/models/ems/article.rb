4cced2e1528ebdb850a30c609df5796447896e65
Vincent Siebert <vincent@siebert.im>
2012-05-22 13:30:14 +0100

    temp fix

--- app/models/ems/article.rb
+++ app/models/ems/article.rb
@@ -51,7 +51,7 @@ module Ems
     accepts_nested_attributes_for :assets, :allow_destroy => true
     
     # paperclip files
-    has_attached_file :image, :styles => { :image564x252 => "564x252#", :image312x189 => "312x189#", :image312x126 => "312x126", :image228x126 => "228x126" }.merge(PAPERCLIP_STORAGE_OPTIONS)
+    has_attached_file :image, :styles => { :image564x252 => "564x252#", :image312x189 => "312x189#", :image312x126 => "312x126", :image228x126 => "228x126" }
 
 
     # Method to make sure we have all our default values set on the object
