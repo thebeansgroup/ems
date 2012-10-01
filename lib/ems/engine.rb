4cd778e43918899c6285e5969f8d4ba0dc24c6f7
Vincent Siebert <vincent@siebert.im>
2012-10-01 16:57:20 +0100

    making sure the ems version file is included in the engine

--- lib/ems/engine.rb
+++ lib/ems/engine.rb
@@ -1,12 +1,13 @@
+require "ems/engine"
+
 module Ems
   class Engine < ::Rails::Engine
     isolate_namespace Ems
     # Add a load path for this specific Engine
     config.autoload_paths << File.expand_path("#{Engine.root}/lib/query_builder/", __FILE__)
     config.autoload_paths << File.expand_path("#{Engine.root}/lib/kramdown", __FILE__)
-    config.generators do |g| 
-      g.template_engine :haml 
-    end 
-    
+    config.generators do |g|
+      g.template_engine :haml
+    end
   end
 end
